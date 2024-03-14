5b8f40a1b767f26f888a360be40c26c7afcbaf67
Alejandro Otalvaro <jaom2479@gmail.com>
2024-03-14 09:40:38 -0500

    Update
    Update

---
+++ database/migrations/2024_03_14_140845_create_peticions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peticions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->string('tipo_peticion');
+            $table->text('asunto');
+            $table->boolean('estatus')->default(false);
+            $table->dateTime('fecha_vencimineto');
+            $table->timestamps();
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peticions');
+    }
+};
