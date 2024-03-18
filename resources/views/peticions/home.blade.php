e398096ff361f85a01f830eb87213ac1e8cb6b8e
Alejandro Otalvaro <jaom2479@gmail.com>
2024-03-18 08:54:54 -0500

    Update
    Update

--- resources/views/peticions/home.blade.php
+++ resources/views/peticions/home.blade.php
@@ -6,26 +6,34 @@
     <title>Sistema Peticiones</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100">
-    <div class="flex h-screen">
-        <!-- Barra Lateral -->
-        <div class="w-64 bg-gray-800 text-white">
-            <div class="p-4 text-2xl font-bold">Panel Administrador</div>
-            <ul class="py-4">
-                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Home</li>
-                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Crear Peticion</li>
-                <li class="px-4 py-2 hover:bg-gray-700 cursor-pointer">Cerrar Sesión</li>
-            </ul>
-        </div>
-        <!-- Barra Lateral -->
-
-        <!-- Contenido Principal -->
-        <div class="flex-1 p-10">
-            <h1 class="text-3xl font-bold mb-5">Lista de Peticiones</h1>
-            <!-- Example Content -->
-            <div class="bg-white shadow-md rounded-lg p-6">
-                <p>Welcome to the admin panel. This is just a placeholder content.</p>
+<body>
+    <div class="bg-gray-100">
+        <!-- Contenedor Barra y Contenido -->
+        <div class="flex flex-col h-screen md:flex-row">
+            <!-- Barra Lateral -->
+            <div class="w-full bg-sky-600 text-white md:w-64">
+                <div class="p-4 text-2xl font-bold text-center">
+                    Panel Administrador
+                </div>
+                <ul class="flex flex-row justify-center md:flex-col">
+                    <li class="hover:bg-sky-700">
+                        <a class="block px-4 py-2" href="#">Home</a>
+                    </li>                
+                    <li class="hover:bg-sky-700">
+                        <a class="block px-4 py-2" href="#">Crear Petición</a>
+                    </li>    
+                    <li class="hover:bg-sky-700">
+                        <a class="block px-4 py-2" href="#">Cerrar Sesión</a>
+                    </li>    
+                </ul>
             </div>
+            <!-- Barra Lateral -->
+    
+            <!-- Contenido Principal -->
+            <div>
+
+            </div>
+            
         </div>
     </div>
 </body>
