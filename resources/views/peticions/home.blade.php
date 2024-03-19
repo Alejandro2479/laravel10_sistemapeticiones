5fc7a936ec11062fc4b57ef4f012fdca18c26397
Alejandro Otalvaro <jaom2479@gmail.com>
2024-03-19 09:45:57 -0500

    Update
    Update

--- resources/views/peticions/home.blade.php
+++ resources/views/peticions/home.blade.php
@@ -1,45 +1,44 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Peticiones | Home</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-    <!-- Contenedor Barra y Contenido -->
-    <div class="flex flex-col h-screen md:flex-row">
-        <!-- Barra Lateral -->
-        <nav class="bg-sky-600 text-white md:w-64">
+    <!-- BARRA LATERAL -->
+    <div class="flex-none flex-row h-auto w-full bg-sky-500 text-white md:flex md:flex-col md:h-full md:w-64 md:fixed">
+        <nav class="bg-sky-500 text-white md:w-full">
             <h1 class="p-4 text-2xl font-bold text-center">
                 Panel Administrador
             </h1>
-            <ul class="flex flex-row justify-center md:flex-col">
+            <ul class="flex flex-row justify-center font-semibold md:flex-col">
                 <li class="hover:bg-sky-700 duration-500">
                     <a class="block px-4 py-2" href="#">Home</a>
                 </li>                
                 <li class="hover:bg-sky-700 duration-500">
                     <a class="block px-4 py-2" href="{{ route('peticions.crear-peticion') }}">Crear Petición</a>
-                </li>    
-                <li class="hover:bg-sky-700 duration-500">
-                    <a class="block px-4 py-2" href="{{ route('peticions.crear-usuario') }}">Crear Usuario</a>
-                </li>    
+                </li>
                 <li class="hover:bg-sky-700 duration-500">
                     <a class="block px-4 py-2" href="#">Cerrar Sesión</a>
                 </li>    
             </ul>
         </nav>
-        <!-- Barra Lateral -->
-    
-        <!-- Contenido Principal -->
-        <div class="mx-auto mt-10 mb-10">
-            <div class="mx-4">
-                <h1 class="text-2xl mb-4 font-medium">Lista de Peticiones</h1>                
-                <!-- Tabla -->
-                <table class="w-full table-auto border border-slate-500 overflow-x-scroll">
+    </div>
+    <!-- BARRA LATERAL -->
+
+    <!-- CONTENIDO PRINCIPAL -->
+    <div class="md:ml-64">
+        <div class="p-4">
+            <h2 class="text-2xl font-semibold mb-2">Lista de Petciones</h2>
+            <div class="overflow-x-auto">
+                <table class="w-full table-auto">
                     <thead>
-                        <tr class="border border-slate-600">
-                            <th class="px-4 py-2">Numero de Radicado</th>
+                        <tr class="bg-gray-200">
+                            <th class="px-4 py-2">Número de Radicado</th>
                             <th class="px-4 py-2">Asunto</th>
                             <th class="px-4 py-2">Dias Vencimiento</th>
                             <th class="px-4 py-2">Estatus</th>
@@ -48,19 +47,21 @@
                     </thead>
                     <tbody>
                         @forelse($peticions as $peticion)
-                        <tr class="border-t border-slate-500">
+                        <tr class="border-t">
                             <td class="px-4 py-2 text-center">{{ $peticion->numero_radicado }}</td>
                             <td class="px-4 py-2">{{ $peticion->asunto }}</td>
                             <td class="px-4 py-2 text-center">X</td>
                             <td class="px-4 py-2 text-center">{{ $peticion->estatus ? 'Completa' : 'Incompleta' }}</td>
-                            <td class="flex flex-col px-4 py-2 items-center justify-center space-y-2 space-x-0 md:flex-row md:space-y-0 md:space-x-2">
-                                <button class="block w-full bg-lime-600 text-white px-4 py-1 rounded-lg hover:bg-lime-700 duration-500">
-                                    <a href="#">Ver</a>
-                                </button>
+                            <td class="flex flex-col px-4 py-2 items-center justify-center space-y-2 space-x-0 text-white font-semibold md:flex-row md:space-y-0 md:space-x-2">
+                                <a href="#" class="block w-full">
+                                    <button class="py-2 px-4 rounded bg-sky-500 hover:bg-sky-700 duration-500 w-full">
+                                        Ver
+                                    </button>
+                                </a>                                
                                 <form action="{{ route('peticions.eliminar', ['peticion' => $peticion->id]) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button class="bg-red-600 text-white px-4 py-1 rounded-lg hover:bg-red-700 duration-500">
+                                    <button class="py-2 px-4 rounded bg-red-500 hover:bg-red-700 duration-500">
                                         Eliminar
                                     </button>
                                 </form>
@@ -68,18 +69,20 @@
                         </tr>
                         @empty
                         <tr>
-                            <td class="px-4 py-2" colspan="4">No hay peticiones</td>
+                            <td class="border px-4 py-2" colspan="5">No hay peticiones</td>
                         </tr>
                         @endforelse
-                        </tbody>
-                    </table>
-                @if ($peticions->count())
-                <nav class="mt-4">
-                    {{ $peticions->links() }}
-                </nav>
-                @endif
+                    </tbody>
+                </table>
             </div>
+            @if ($peticions->count())
+            <nav class="mt-4">
+                {{ $peticions->links() }}
+            </nav>
+            @endif
         </div>
     </div>
+    <!-- CONTENIDO PRINCIPAL -->
 </body>
+
 </html>
