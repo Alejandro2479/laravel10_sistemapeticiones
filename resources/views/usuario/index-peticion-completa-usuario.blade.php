93b5a594df799e7bc52b81c91ee6a09aa6a4ec18
Alejandro Otalvaro <jaom2479@gmail.com>
2024-04-08 15:25:20 -0500

    Update
    Update

--- resources/views/usuario/index-peticion-completa-usuario.blade.php
+++ resources/views/usuario/index-peticion-completa-usuario.blade.php
@@ -14,6 +14,7 @@
                             <th class="px-4 py-2">Asunto</th>
                             <th class="px-4 py-2">Dias Vencimiento</th>
                             <th class="px-4 py-2">Estatus</th>
+                            <th class="px-4 py-2">Fecha Ingreso</th>
                             <th class="px-4 py-2">Acciones</th>
                         </tr>
                     </thead>
@@ -24,6 +25,7 @@
                             <td class="px-4 py-2">{{ $peticion->asunto }}</td>
                             <td class="px-4 py-2 text-center">X</td>
                             <td class="px-4 py-2 text-center">{{ $peticion->estatus ? 'Completa' : 'Incompleta' }}</td>
+                            <td class="px-4 py-2 text-center">{{ $peticion->created_at->format('d/m/Y') }}</td>
                             <td class="flex flex-col px-4 py-2 items-center justify-center space-y-2 space-x-0 md:flex-row md:space-y-0 md:space-x-2">
                                 <a href='{{ route('usuario.peticion-mostrar', ['peticion' => $peticion->id]) }}'>
                                     <button class="py-2 px-4 rounded bg-sky-500 text-white font-semibold hover:bg-sky-600 duration-500 w-full">Ver</button>
