c004a3a5922fa7af38ba624706f4a3d4c88f75c1
Alejandro Otalvaro <jaom2479@gmail.com>
2024-04-17 15:47:06 -0500

    Update
    Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,9 +14,9 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('peticiones:actualizar-dias')->daily();
     }
 
     /**
