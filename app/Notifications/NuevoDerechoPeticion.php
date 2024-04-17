6aeeb7699e65fec034e873e8c74b8b11c23695f5
Alejandro Otalvaro <jaom2479@gmail.com>
2024-04-17 09:53:41 -0500

    Update
    Update

---
+++ app/Notifications/NuevoDerechoPeticion.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Carbon\Carbon;
+
+class NuevoDerechoPeticion extends Notification
+{
+    use Queueable;
+
+    protected $numeroRadicado;
+    protected $fechaVencimiento;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($numeroRadicado, $fechaVencimiento)
+    {
+        $this->numeroRadicado = $numeroRadicado;
+        $this->fechaVencimiento = $fechaVencimiento;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $fechaVencimientoFormateada = Carbon::parse($this->fechaVencimiento)->format('d/m/Y');
+
+        return (new MailMessage)
+            ->subject('Nuevo derecho de petición asignado')
+            ->line('Se le asignó un derecho de petición con número de radicado ' . $this->numeroRadicado . ' el cual vence el día ' . $fechaVencimientoFormateada);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
