0a26912577e10f004c22caa657ddd3578aa115dc
Alejandro Otalvaro <jaom2479@gmail.com>
2024-05-21 13:34:30 -0500

    Update
    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,12 @@ class UserController extends Controller
         $peticiones = Peticion::when(
             $numeroRadicado,
             fn ($query, $numeroRadicado) => $query->numeroRadicado($numeroRadicado)
-        )->where([['user_id', $userId], ['estatus', false]])->oldest()->paginate(10);
+        )->where('estatus', false)
+            ->whereHas('users', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })
+            ->oldest()
+            ->paginate(10);
 
         return view('user.index-peticion-user', ['peticiones' => $peticiones]);
     }
@@ -27,13 +32,17 @@ class UserController extends Controller
     public function indexPeticionCompleta(Request $request)
     {
         $userId = Auth::id();
-
         $numeroRadicado = $request->input('numero_radicado');
 
         $peticiones = Peticion::when(
             $numeroRadicado,
             fn ($query, $numeroRadicado) => $query->numeroRadicado($numeroRadicado)
-        )->where([['user_id', $userId], ['estatus', true]])->oldest()->paginate(10);
+        )->where('estatus', true)
+            ->whereHas('users', function ($query) use ($userId) {
+                $query->where('user_id', $userId);
+            })
+            ->oldest()
+            ->paginate(10);
 
         return view('user.index-peticion-completa-user', ['peticiones' => $peticiones]);
     }
